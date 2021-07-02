5d0a406b81627b275036b42355a7f5aaf82ea2d5
Arne Dalhuisen <59421074+Bloepiloepi@users.noreply.github.com>
2021-07-02 23:47:42 +0200

    Convert to extension

---
+++ src/main/java/io/github/bloepiloepi/particles/ParticlesMain.java
@@ -0,0 +1,15 @@
+package io.github.bloepiloepi.particles;
+
+import net.minestom.server.extensions.Extension;
+
+public class ParticlesMain extends Extension {
+	@Override
+	public void initialize() {
+	
+	}
+	
+	@Override
+	public void terminate() {
+	
+	}
+}
