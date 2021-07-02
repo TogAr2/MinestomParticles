89c0e6fc5ed89db1a65071b74d38be274585f8d6
Arne Dalhuisen <59421074+Bloepiloepi@users.noreply.github.com>
2021-07-02 23:59:56 +0200

    Added extension test

---
+++ src/test/java/TestMainBoot.java
@@ -0,0 +1,8 @@
+import net.minestom.server.Bootstrap;
+
+//Yes I am very good at names
+public class TestMainBoot {
+	public static void main(String[] args) {
+		Bootstrap.bootstrap("TestMain", args);
+	}
+}
